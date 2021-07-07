d7f089c3fc01ce596282f456392c9efd76d62794
lee93 <dohuny2@naver.com>
2021-07-07 16:40:06 +0900

    20210707

--- JavaFX_Movie/src/application/Main.java
+++ JavaFX_Movie/src/application/Main.java
@@ -1,19 +1,22 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent parent = FXMLLoader.load(getClass().getResource("/FXML/Main.fxml") );
+			Scene scene = new Scene(parent);
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("MEGA CINEMA : select movie");
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
