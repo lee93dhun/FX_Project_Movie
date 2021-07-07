1cbe009e0ceea845433f6aa548f12b14d2ffb5d3
lee93 <dohuny2@naver.com>
2021-07-07 16:15:08 +0900

    20210707

--- JavaFX_Movie/src/controller/Infocontroller.java
+++ JavaFX_Movie/src/controller/Infocontroller.java
@@ -1,5 +1,48 @@
 package controller;
 
-public class Infocontroller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class Infocontroller implements Initializable{
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@FXML
+    private ImageView imgselectimg;
+
+    @FXML
+    private Label lblselecttitle;
+    
+    @FXML
+    private Label lblselectgenre;
+
+    @FXML
+    private Label lblselecttime;
+
+    @FXML
+    private Label lblselectps;
+
+    @FXML
+    private Label lblselectseat;
+
+    @FXML
+    private Button btnclose;
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
 
 }
--- JavaFX_Movie/src/controller/Maincontroller.java
+++ JavaFX_Movie/src/controller/Maincontroller.java
@@ -1,5 +1,53 @@
 package controller;
 
-public class Maincontroller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class Maincontroller implements Initializable {
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@FXML
+    private ImageView img_1;
+
+    @FXML
+    private ImageView img_2;
+
+    @FXML
+    private ImageView img_3;
+
+    @FXML
+    private Button btnticketing_1;
+
+    @FXML
+    private Button btnticketing_2;
+
+    @FXML
+    private Button btnticketing_3;
+
+    @FXML
+    void ticketing_1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ticketing_2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ticketing_3(ActionEvent event) {
+
+    }
 
 }
